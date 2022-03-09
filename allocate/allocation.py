57ce44debeb1491351cfc1cdff39a2872fa0dfe2
Nick Santos <nickrsan@users.noreply.github.com>
2022-03-08 20:01:35 -0800

    Monte Carlo controller starting to take shape, but still incomplete

--- allocate/allocation.py
+++ allocate/allocation.py
@@ -1,4 +1,8 @@
 from collections import defaultdict
+from itertools import combinations
+import math
+
+import numpy
 
 from cvxpy import Problem, Variable, Parameter, Maximize, sum as cvxsum
 from . import models
@@ -60,6 +64,7 @@ def get_parts(cost_timestep=1, year=2018, service_area=None, use_crop_constraint
             vars_by_well[well.well_id].append(variable)
             vars_by_field[field.liq_id].append(variable)
 
+    irrigation_efficiency_params = {}
     for field in vars_by_field:  # for each field, make sure the allocations to it are less than the demand
         #log.info(f"Field: {field}, timestep: {cost_timestep}")
         try:
@@ -68,7 +73,8 @@ def get_parts(cost_timestep=1, year=2018, service_area=None, use_crop_constraint
             field_demand = 0  # if we don't know the demand for the field, assume it wasn't planted and allocate 0 applied water
 
         log.info(f"Field Demand: {field_demand}")
-        demand = float(field_demand) / Parameter(name=f"{field}_irrigation_efficiency", value=0.75)
+        irrigation_efficiency_params[field] = Parameter(name=f"{field}_irrigation_efficiency", value=0.75)
+        demand = float(field_demand) / irrigation_efficiency_params[field]
         demands_by_field[field] = demand
         constraints.append(cvxsum(vars_by_field[field]) <= demand)  # make sure it doesn't go very high - should maybe do this with the benefit function and not a constraint
         constraints.append(cvxsum(vars_by_field[field]) >= FIELD_DEMAND_MARGIN * demand)  # make sure that we get close to the amount of water required. Leaving a bit of slosh to allow for data misalignments
@@ -92,7 +98,14 @@ def get_parts(cost_timestep=1, year=2018, service_area=None, use_crop_constraint
         if use_crop_constraints:
             set_crop_constraints(constraints, vars_by_name, well, well_obj)
 
-    return {"benefits": benefits, "costs": costs, "constraints": constraints, "vars_by_well": vars_by_well, "vars_by_field": vars_by_field, "demands_by_field": demands_by_field}
+    return {"benefits": benefits,
+            "costs": costs,
+            "constraints": constraints,
+            "vars_by_well": vars_by_well,
+            "vars_by_field": vars_by_field,
+            "demands_by_field": demands_by_field,
+            "irrigation_efficiency_params": irrigation_efficiency_params
+            }
 
 
 def set_crop_constraints(constraints, vars_by_name, well, well_obj):
@@ -113,6 +126,7 @@ def build_problem(service_area=None, use_crop_constraints=True):
     problem_info = get_parts(service_area=service_area, use_crop_constraints=use_crop_constraints)
     problem = Problem(Maximize(cvxsum(problem_info["benefits"]) - cvxsum(problem_info["costs"])), problem_info["constraints"])
     return problem, problem_info
+
 
 def solve_and_report(problem, problem_info):
     problem.solve(verbose=True)
@@ -139,12 +153,37 @@ def solve_and_report(problem, problem_info):
 
 
 class MonteCarloController(object):
+    """
+        We'll run a Monte Carlo for each service area - we'll check the number of fields and the number of
+        irrigation options for each field - if the set of combinations is small, we'll just run a brute
+        force of all combinations. E.g. 6 fields with 3 irrigation types each should just be brute forced.
+        If it's large, we'll run a Monte Carlo - maybe with 1000 iterations? We'll see how fast they run
+        once we can get them parameterized and then how good it is.
+
+        We'll validate the approach and verify results against some service areas that we calculate the
+        most likely irrigation combinations by hand.
+
+        For each type, we'll start with a prior probability of the irrigation type for each field based on
+        the landiq crop in the field, which we'll use when selecting irrigation type mixes. We'll then
+        score each mix of irrigation types based on how much (or little) error it contains in the final
+        mass balance when we allocate water from different pipes. We can increase the likelihood of
+        getting the right irrigation type by using multiple timesteps (by month, which we don't
+        *always* have - but we should consider using it for the fields that have it), where maybe
+        an irrigation type completes the mass balance well the first month, but poorly in the next,
+        but another irrigation type does slightly worse the first month, but much better the second.
+        We'd probably prefer the second at that point.
+
+        We'll then do something akin to a bayesian update, where we'll adjust the probabilities of each
+        field's irrigation type according to the error values.
+    """
     service_area = None
     fields = list()
     field_efficiencies = dict()
     problem = None
     problem_info = None
     use_crop_constraints = True
+    monte_carlo_iterations = 1000
+    brute_force_combinations_threshold = 100
 
     def __init__(self, service_area_id, use_crop_constraints):
         self.service_area = service_area_id
@@ -154,11 +193,59 @@ class MonteCarloController(object):
     def build(self):
         self.problem, self.problem_info = build_problem(self.service_area, use_crop_constraints=self.use_crop_constraints)
 
-    def run(self, iterations=1000):
-        pass
+    def run(self, iterations=None):
+        if iterations is None:
+            iterations = self.monte_carlo_iterations
 
-    def run_iteration(self):
-        pass
+        efficiency_information = self.get_combinations()
+        for iteration in range(iterations):
+            self.run_iteration(efficiency_information=efficiency_information)
+
+    def get_combinations(self):
+        fields = models.AgField.objects.filter(ucm_service_area_id=self.service_area)
+        irrigation_nums = []
+        field_irrigation_options = {}
+
+        # we don't technically need to collapse this into a custom object here, but I think it might help to
+        # avoid random hits to the DB later on.
+        for field in fields:
+            priors = field.crop.irrigation_priors
+            irrigation_nums.append(len(priors))
+            field_irrigation_options[field.liq_id] = {
+                'liq_id': field.liq_id,
+                'crop_name': field.crop.vw_crop_name,
+                'crop_id': field.crop_id,
+                'irrigation': [{
+                    'irrigation_id': prior.irrigation_type.id,
+                    'prior_id': prior.id,
+                    'name': prior.irrigation_type.name,
+                    'efficiency': prior.irrigation_type.efficiency,
+                    'probability': prior.probability
+                } for prior in priors],
+            }
+
+            # we'll want to use numpy.random.choice, and for that we need lists of the efficiencies to choose and their individual probabilities - cache these so
+            # that we don't run the list comprehension every time. Though we'll need to update the list of probabilities when we do Bayesian updates
+            field_irrigation_options[field.liq_id]["efficiencies"] = [item.efficiency for item in field_irrigation_options[field.liq_id]["irrigation"]]
+            field_irrigation_options[field.liq_id]["probabilities"] = [item.probability for item in field_irrigation_options[field.liq_id]["irrigation"]]
+
+        return field_irrigation_options
+        #num_combinations = math.prod(irrigation_nums)
+        #if num_combinations > self.brute_force_combinations_threshold:
+        #    return None
+        #else:
+        #    return
+
+    def run_iteration(self, efficiency_information):
+        # for each iteration, set new irrigation efficiencies for each field by choosing from the available options
+        # based on their probability
+        for field in self.problem_info["irrigation_efficiency_params"]:
+            field_param = self.problem_info["irrigation_efficiency_params"][field]
+            field_options = efficiency_information[field]
+            field_param.value = numpy.random.choice(field_options["efficiencies"], replace=True, p=field_options["probabilities"])
+
+
+
 
     def update_results(self):
         pass
